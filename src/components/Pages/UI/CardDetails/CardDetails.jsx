95e690233ecef41adee50cf1e2c5e420470a66e3
rahul-tech1234 <rahul.laravel.dev@gmail.com>
2026-05-29 21:03:09 +0600

    design for all device

--- src/components/Pages/UI/CardDetails/CardDetails.jsx
+++ src/components/Pages/UI/CardDetails/CardDetails.jsx
@@ -26,14 +26,12 @@ const Buttons=<>
     return (
         <>
 <div className="bg-gray-200 card mp-3">
-        <div className="grid md:grid-rows-4 md:grid-cols-5 w-10/12 mx-auto gap-3">
-            <div className="row-span-4 col-span-2 p-3 ">
-
-
+        <div className="grid grid-cols-1 grid-rows-1 md:grid-rows-4 md:grid-cols-5 w-10/12 mx-auto gap-3">
+            <div className="row-span-4 col-span-5 sm:col-span-2 p-3 ">
                 <div className="card text-center shadow-lg  space-y-2 py-2 px-1 bg-white">
-                <img src={picture} alt={name} className="w-25 mx-auto rounded-full"/>
-                <h2 className="text-[#1F2937] font-bold">{name}</h2>
-                <span className={`${status==='active'?'bg-[#EFAD44]':status==='on-track'?'bg-[#244D3F]':'bg-[#EF4444]'} rounded-2xl px-1.5 py-0.5 text-white mx-auto badge`} >{status}</span>
+                    <img src={picture} alt={name} className="w-25 mx-auto rounded-full"/>
+                    <h2 className="text-[#1F2937] font-bold">{name}</h2>
+                    <span className={`${status==='active'?'bg-[#EFAD44]':status==='on-track'?'bg-[#244D3F]':'bg-[#EF4444]'} rounded-2xl px-1.5 py-0.5 text-white mx-auto badge`} >{status}</span>
                 <div>
                     { 
                         tags.map((tag,i)=><span key={i} className="badge bg-green-200 rounded-2xl">{tag}</span>)
@@ -42,20 +40,13 @@ const Buttons=<>
                     <p className="text-[#64748B]">"{bio}"</p>
                     <p className="text-[#64748B]">{email}</p> 
                     <div className="space-y-5">{Buttons}</div>
-
-        </div>
-
-
-
-
+                </div>
             </div>
             
                 
-                <div className="card shadow py-3 text-center mt-3 bg-white">
+            <div className="card shadow py-3 text-center mt-3 bg-white">
                 <div className="text-[32px] font-semibold text-[#244D3F]">{days_since_contact}</div>
                 <div className="text-[#64748B] text-[18px font-regular]">Days Since Contact</div>
-            
-
             </div>
             
 
